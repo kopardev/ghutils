7c2a0c8f93ddef7c6aeaa9ea1fc6494fbf6db9d3
Vishal Koparde <vishal.koparde@nih.gov>
2022-06-02 21:47:20 -0400

    adding src files

---
+++ src/fileutils.py
@@ -0,0 +1,21 @@
+import os
+
+def exe_in_path(cmd, path=None):
+    """Checks if an executable is in $PATH
+    @param cmd <str>:
+        Name of executable to check
+    @param path <list>:
+        Optional list of PATHs to check [default: $PATH]
+    @return <boolean>:
+        True if exe in PATH, False if not in PATH
+    """
+    if path is None:
+        path = os.environ["PATH"].split(os.pathsep)
+
+    for prefix in path:
+        filename = os.path.join(prefix, cmd)
+        executable = os.access(filename, os.X_OK)
+        is_not_directory = os.path.isfile(filename)
+        if executable and is_not_directory:
+            return True
+    return False
